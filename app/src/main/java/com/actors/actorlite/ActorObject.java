670644bad8615048d379c63c75a70e18e3a6f6b4
Ahmed Adel Ismail <ahm3d.ad3l@gmail.com>
2017-12-26 04:08:34 +0200

    added sample Object for documentation only

---
+++ app/src/main/java/com/actors/actorlite/ActorObject.java
@@ -0,0 +1,40 @@
+package com.actors.actorlite;
+
+import android.support.annotation.NonNull;
+
+import com.actors.Actor;
+import com.actors.ActorScheduler;
+import com.actors.ActorSystem;
+import com.actors.Message;
+
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * a sample Object for documentation purpose
+ * <p>
+ * Created by Ahmed Adel Ismail on 12/26/2017.
+ */
+public class ActorObject implements Actor {
+
+    public ActorObject() {
+        ActorSystem.register(this);
+    }
+
+    @NonNull
+    @Override
+    public Scheduler observeOnScheduler() {
+        return Schedulers.computation();
+    }
+
+
+    @Override
+    public void onMessageReceived(Message message) {
+        // ...
+    }
+
+    public void onDestroy() {
+        ActorSystem.unregister(this);
+        ActorScheduler.cancel(getClass());
+    }
+}
